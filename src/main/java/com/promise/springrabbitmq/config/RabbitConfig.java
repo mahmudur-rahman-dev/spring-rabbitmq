1fb64ef1108a5543e6ee57ee3432958e11146b3d
Mahmudur Rahman Promise <mahmudur.rahman.csedu@gmail.com>
2023-05-22 18:20:06 +0600

    add configuration for message conversion on rabbit config class

--- src/main/java/com/promise/springrabbitmq/config/RabbitConfig.java
+++ src/main/java/com/promise/springrabbitmq/config/RabbitConfig.java
@@ -1,10 +1,10 @@
 package com.promise.springrabbitmq.config;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
-import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -37,6 +37,15 @@ public class RabbitConfig {
                 .with(bindingKey);
     }
 
-    //Connection Factory, Rabbit Template, Rabbit Admin are automatically configured by the spring boot.
+    @Bean
+    public MessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
 
+    @Bean
+    public AmqpTemplate template(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(messageConverter());
+        return rabbitTemplate;
+    }
 }
